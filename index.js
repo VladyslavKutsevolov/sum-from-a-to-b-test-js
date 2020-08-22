113b920f2a529bf130c0f18606d9b468d2c34ab4
Vladyslav <kytsevolov@gmail.com>
2020-08-21 18:37:01 -0600

    sum recursively

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-
 function sum(fromN, toN) {
   // Sum all the values from fromN up to toN
+  let result = 0;
+  if (toN === 0) {
+    return 0;
+  }
+
+  if (toN === 1) {
+    return 1;
+  }
+
+  if (fromN <= toN) {
+    result += toN + sum(fromN, toN - 1);
+  }
+  return result;
 }
+
+console.log(sum(3, 7));
 
 module.exports = sum;
